4e2406d5129156946f101ebeb690ceebdc9a0f7e
tanngo729 <tanngo729@gmail.com>
2025-07-28 09:42:50 +0700

    Bai6-primitive-data-types

---
+++ 6.Bai6-Primitive-data-types/main.js
@@ -0,0 +1,25 @@
+// Kiểu number
+let soNguyen = 19;
+let soThuc = 3.14;
+
+// Kiểu boolean
+let check = true;
+
+// Kiểu undefined
+let diemToan;
+
+// Kiểu null
+let connect = null;
+
+// Xuất giá trị của biến
+console.log(soNguyen);
+console.log(soThuc);
+console.log(typeof soNguyen);
+console.log(typeof soThuc);
+console.log(typeof check);
+console.log(typeof diemToan);
+console.log(typeof connect);
+
+// Số nguyên an toàn tối đa và tối thiểu
+console.log("Số nguyên an toàn tối đa: ", Number.MAX_SAFE_INTEGER);
+console.log("Số nguyên an toàn tối thiểu: ", Number.MIN_SAFE_INTEGER);

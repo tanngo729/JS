7ff3d60a6eb0e89f26f97d0e6e17d85fa617e72b
tanngo729 <tanngo729@gmail.com>
2025-07-28 09:21:21 +0700

    Bai5-bien-js

---
+++ 5.Bai5-bien-js/main.js
@@ -0,0 +1,33 @@
+// alert("xin cc");
+// prompt("mời nhập tên của bạn: ");
+
+
+// Khai báo biến
+// var name;
+// let yourName;
+
+// Khởi tạo biến
+// var diemToan = 10;
+// const doSoi = 100;
+// let fisrtName = "Alice";
+
+// Xuất giá trị của biến
+// console.log(diemToan);
+// console.log("Điểm toán của bạn là: " + diemToan + " điểm");
+// console.log(doSoi);
+// console.log(fisrtName);
+
+// Thử thay đổi giá trị của biến
+// diemToan = 5;
+// console.log("Điểm toán của bạn là: " + diemToan + " điểm");
+// fisrtName = "Jacky";
+// console(fisrtName);
+
+// Thử gán lại biến hằng số
+// doSoi = 50;
+// console.log(doSoi);
+
+// Bài tập
+let name = prompt("Nhập tên của bạn");
+console.log("Tôi tên là: ", name);
+console.log("Chào mừng bạn đến với bình nguyên vô tận");

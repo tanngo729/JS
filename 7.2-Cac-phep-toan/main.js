c9142d938c4132a391943bef55999855d4e2e524
tanngo729 <tanngo729@gmail.com>
2025-07-28 14:41:33 +0700

    Bai7.2-cac-phep-toan

---
+++ 7.2-Cac-phep-toan/main.js
@@ -0,0 +1,15 @@
+// 7.2 - Các phép toán cơ bản
+let a = 5, b = 2;
+
+let tong = a + b;
+let hieu = a - b;
+let tich = a * b;
+let thuong = a / b;
+let layDu = a % b;
+
+// Xuất kết quả
+console.log(`Tổng 2 số: ${tong}`);
+console.log(`Hiệu 2 số: ${hieu}`);
+console.log(`Tích 2 số: ${tich}`);
+console.log(`Thương 2 số: ${thuong}`);
+console.log(`Chia lấy dư 2 số: ${layDu}`);

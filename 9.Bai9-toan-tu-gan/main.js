fa50e2aee022bd38b1506a513ba060c814ab9d67
tanngo729 <tanngo729@gmail.com>
2025-07-30 08:51:12 +0700

    Bai9-toan-tu-gan

---
+++ 9.Bai9-toan-tu-gan/main.js
@@ -0,0 +1,53 @@
+// // 09. Toán tử gán
+// // 1- toán tử gán (=) dùng để gán giá trị cho biến.
+
+// let x = 10; // Biến x được gán giá trị là 10
+// let y = 5; // Biến y được gán giá trị là 5
+// let z = x + y; // Biến z được gán giá trị là tổng của x và y
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+
+// // Gán +=, -=, *=, /=, %=
+// console.log(`Kết quả gán +=, -=, *=,/= ,%=`);
+// let m = 10;
+// m += 5; // Tương đương với m = m + 5;
+// console.log(m);
+
+// let n = 8;
+// n -= 3;// Tương đương với n = n- 3;
+// console.log(n);
+
+// let p = 6;
+// p *= 2; // Tương đương với p = p * 2;
+// console.log(p);
+
+// let q = 9;
+// q /= 3; // Tương đương với q = q /3;
+// console.log(q);
+
+// let r = 4;
+// r %= 3;// Tương đương với r %=3;
+// console.log(r);
+
+// // Bài tập
+let a = 7;
+a += 3;
+console.log(`a =`, a); // 10
+
+let b = 15
+b -= 6;
+console.log(`b = `, b); //9
+
+let c = 5;
+c *= 4;
+console.log(`c = `, c); // 20
+
+let d = 12;
+d /= 2;
+console.log(`d = `, d); //6
+
+let e = 5, f = 2;
+e -= f + 1;
+console.log(`e = `, e); // 2

568bc7a2409ec23da148215dba36cdfcb983266c
tan ngo <tanngo729@gmail.com>
2025-12-30 16:03:51 +0700

    operator

---
+++ src/Bai-4-Operators/Bai4.js
@@ -0,0 +1,10 @@
+console.log("Bài 4 - Toán tử");
+
+let a = 5;
+const b = ++a;// ++a => tăng ngay lập tức
+//a++ => tăng sau khi sử dụng
+const c = --a;// c = 6, a = 5
+
+console.log("a = " + a);
+console.log("b = " + b);
+console.log("c = " + c);

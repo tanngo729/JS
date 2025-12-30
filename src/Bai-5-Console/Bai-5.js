b6738ae34e0592e59bff1471ff2209dd8fdc7335
tan ngo <tanngo729@gmail.com>
2025-12-30 16:24:11 +0700

    console

---
+++ src/Bai-5-Console/Bai-5.js
@@ -0,0 +1,13 @@
+console.log("Bài 5 console");
+// console.log("info");
+// console.warn("test warning");
+// console.error("test error");
+
+let name = "ngo truong tan";
+let age = 21;
+
+// không nên: cộng chuỗi dễ sai
+console.log("Name: " + name + ", Age: " + age);
+
+// nên dùng:
+console.log("Name:", name, ", Age:", age);
